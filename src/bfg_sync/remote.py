a0966e142f91e4867f5e28d111265415179aabe8
Jeff <jeffwatkins2000@gmail.com>
2025-11-25 13:51:39 +0000

    Create SSHClient class

--- src/bfg_sync/remote.py
+++ src/bfg_sync/remote.py
@@ -19,45 +19,45 @@ key_filename = os.getenv('PRIVATE_KEY_FILE')
 known_hosts_file_name = os.getenv('KNOWN_HOSTS')
 
 
-def _get_ssh_client() -> paramiko.client.SSHClient:
-    ssh_client = paramiko.client.SSHClient()
-    ssh_client.load_host_keys(known_hosts_file_name)
-    ssh_client.set_missing_host_key_policy(paramiko.RejectPolicy())
-    try:
-        ssh_client.connect(
-            hostname=hostname,
-            username=username,
-            port=port,
-            key_filename=key_filename,
-            auth_timeout=5,
-            )
-        return ssh_client
-    except paramiko.ssh_exception.AuthenticationException:
-        logger.warning('Authentication error - not connected to remote server')
-    except socket.gaierror:
-        logger.warning('Socket error - not connected to remote server')
-    return None
+class SSHClient(paramiko.client.SSHClient):
+    def __init__(self):
+        super().__init__()
+        self.load_host_keys(known_hosts_file_name)
+        self.set_missing_host_key_policy(paramiko.RejectPolicy())
+        try:
+            self.connect(
+                hostname=hostname,
+                username=username,
+                port=port,
+                key_filename=key_filename,
+                auth_timeout=5,
+                )
+        except paramiko.ssh_exception.AuthenticationException:
+            logger.error(
+                'Authentication error - not connected to remote server')
+        except socket.gaierror:
+            logger.error('Socket error - not connected to remote server')
+
+        self.sftp_client = self.open_sftp()
 
 
 def execute_remote_command(command: str) -> str:
-    if ssh_client := _get_ssh_client():
-        _stdin, _stdout, _stderr = ssh_client.exec_command(command)
-        if _stderr.read().decode():
-            logger.warning('Server side error', err=_stderr.read().decode())
-        ssh_client.close()
-        return _stdout.read().decode()
-    return ''
+    ssh_client = SSHClient()
+
+    _stdin, _stdout, _stderr = ssh_client.exec_command(command)
+    if _stderr.read().decode():
+        logger.warning('Server side error', err=_stderr.read().decode())
+    ssh_client.close()
+    return _stdout.read().decode()
 
 
 def get_remote_files(remote_dir: str, local_dir: str, ignore: list = None):
-    ssh_client = _get_ssh_client()
-    sftp_client = ssh_client.open_sftp()
-    _get_remote_files(ssh_client, sftp_client, remote_dir, local_dir, ignore)
+    ssh_client = SSHClient()
+    _get_remote_files(ssh_client, remote_dir, local_dir, ignore)
 
 
 def _get_remote_files(
         ssh_client: paramiko.client.SSHClient,
-        sftp_client,
         remote_dir: str,
         local_dir: str,
         ignore: list = None):
@@ -71,7 +71,7 @@ def _get_remote_files(
 
     logger.info('Open dir', remote=remote_dir, local=local_dir)
 
-    for entry in sftp_client.listdir_attr(remote_dir):
+    for entry in ssh_client.sftp_client.listdir_attr(remote_dir):
         if entry.filename in ignore:
             continue
         remote_path = f'{remote_dir}/{entry.filename}'
@@ -80,6 +80,5 @@ def _get_remote_files(
         if S_ISDIR(mode):
             get_remote_files(remote_path, local_path, ignore)
         elif S_ISREG(mode):
-            sftp_client.get(remote_path, local_path)
+            ssh_client.sftp_client.get(remote_path, local_path)
             logger.info('Download file', name=entry.filename)
-            ...
